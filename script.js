c6b5bb7685e6c724612c72434d68e749a3b005ae
rydomine <70235536+rydomine@users.noreply.github.com>
2021-02-27 12:51:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var shuffleEle = document.body.querySelector(".shuffle");
+var wrapperEle = document.body.querySelector(".wrapper");
+var animals = ["dog", "fish", "cat", "shark", "cat", "dog"];
+
+animals = animals.sort(()=> Math.random()-0.5);
+
+for(var i=0; i<animals.length; i++){
+ animalFarm(animals[i]);
+}
+
+function animalFarm(str){
+   var ele = document.createElement("div");
+  if(str==="cat"){
+    ele.innerHTML=str+" I am a cat";
+  }else if(str==="dog"){
+    ele.innerHTML=str+" borf borf";
+ }else{
+    ele.innerHTML=str+" I am an animal";
+ }
+  
+  wrapperEle.appendChild(ele);
+}
+
+shuffleEle.addEventListener("click", function(){
+location.reload()
+})
